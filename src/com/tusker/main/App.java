971067d6dcc5743919e5bb694940d683b3b3545a
foxhunter24 <tmitul515@gmail.com>
2019-01-13 17:09:01 +0600

    First commit: Pushing it to the master branch

---
+++ src/com/tusker/main/App.java
@@ -0,0 +1,7 @@
+package com.tusker.main;
+
+public class App {
+    public static void main( String[] args ){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/com/tusker/test/AppTest.java
@@ -0,0 +1,13 @@
+package com.tusker.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testMain(){
+        //assertEquals("Hello World","Hello World");
+    }
+}
